4995a64d06e6a5d8058f79f159310e61ed60f597
Chenzhijun <chenyiyuan00@163.com>
2016-03-13 17:49:56 +0800

    add author mapper

---
+++ src/main/java/com/ccniit/graduation/mapper/AuthorMapper.java
@@ -0,0 +1,65 @@
+package com.ccniit.graduation.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
+
+import com.ccniit.graduation.pojo.db.Author;
+
+public interface AuthorMapper {
+
+	/**
+	 * 插入一条新的Author记录
+	 * 
+	 * @param author
+	 * @return author.id
+	 */
+	@Insert("INSERT INTO author(email,password,nickname) VALUES(#{email},#{password},#{nickname})")
+	@SelectKey(before = false, keyProperty = "id", resultType = Long.class, statement = {
+			"SELECT LAST_INSERT_ID() AS id" })
+	long insertAuthor(Author author);
+
+	/**
+	 * 按照id查询一条Author记录
+	 * 
+	 * @param author.id
+	 * @return author
+	 */
+	@Select("SELECT email,tel,nickname,name,sex,avatar FROM author WHERE id=#{id}")
+	Author selectById(long id);
+
+	/**
+	 * 按照email查询Author的id
+	 */
+	@Select("SELECT id FROM author WHERE email=#{email}")
+	long selectAuthorIdByEmail(String email);
+
+	/**
+	 * 按照email查询一条Author记录
+	 * 
+	 * @param author.email
+	 * @return author
+	 */
+	@Select("SELECT id,tel,nickname,name,sex,avatar FROM author WHERE email=#{email}")
+	Author selectByEmail(String email);
+
+	/**
+	 * 更新一条author记录，按照id
+	 * 
+	 * @param author
+	 * @return affected rows
+	 */
+	@Update("UPDATE author SET tel=#{tel},nickname=#{nickanem),name=#{name},sex=#{sex} WHERE id=#{id}")
+	int updateAuthor(Author author);
+
+	/**
+	 * 统计Email在Author中的记录条数
+	 * 
+	 * @param email
+	 * @return email存在的条数
+	 */
+	@Select("SELECT COUNT(email) FROM author WHERE email=#{email}")
+	int countByEmail(String email);
+
+}
---
+++ src/main/java/com/ccniit/graduation/pojo/db/Author.java
@@ -0,0 +1,79 @@
+package com.ccniit.graduation.pojo.db;
+
+import java.io.Serializable;
+
+public class Author implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3887017416615097662L;
+	private long id;
+	private String email;
+	private String tel;
+	private String password;
+	private String nickname;
+	private String name;
+	private String sex;
+
+	public Author() {
+		super();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}

f0ca05a0c5dc98299b8d7e95eeb0235e5f39eafb
Chenzhijun <chenyiyuan00@163.com>
2016-04-22 17:23:13 +0800

    do email sender

--- src/main/java/com/ccniit/graduation/plus/emil/EmailProvider163.java
+++ src/main/java/com/ccniit/graduation/plus/emil/EmailProvider163.java
@@ -3,11 +3,16 @@ package com.ccniit.graduation.plus.emil;
 import javax.mail.Authenticator;
 
 import org.apache.commons.mail.DefaultAuthenticator;
+import org.springframework.stereotype.Component;
 
+@Component("emailProvider163")
 public class EmailProvider163 extends EmailServerProvider {
+
+	private static final String PROVIDER_NAME = "网易邮箱";
 
 	private static final String HOST_NAME = "smtp.163.com";
 	private static final int SMTP_PORT = 465;
+	// TODO update to real password
 	private static final String USER_NAME = "chenyiyuan00";
 	private static final String PASSWORD = "Zhijun944586";
 	private static final String FROM = "chenyiyuan00@163.com";
@@ -22,7 +27,7 @@ public class EmailProvider163 extends EmailServerProvider {
 
 	@Override
 	public String getName() {
-		return "EastVote";
+		return PROVIDER_NAME;
 	}
 
 }
--- src/main/java/com/ccniit/graduation/plus/emil/EmailService.java
+++ src/main/java/com/ccniit/graduation/plus/emil/EmailService.java
@@ -1,9 +1,22 @@
 package com.ccniit.graduation.plus.emil;
 
+import java.util.List;
+
 import org.apache.commons.mail.EmailException;
 
 public interface EmailService {
 
-	void sendMsg(String to, String subject, String msg) throws EmailException;
+	/**
+	 * 向单个邮箱发送邮件
+	 * @throws EmailException 
+	 */
+	void sendMessage(EmailTemplate template) throws EmailException;
+
+	/**
+	 * 向多个邮箱发送相同的邮件
+	 * 
+	 * @throws EmailException
+	 */
+	void sendMessages(EmailTemplate template, List<String> receivers) throws EmailException;
 
 }

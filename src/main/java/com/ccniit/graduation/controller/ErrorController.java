4d8aca433a967e6763dbab700a6b517f0855d0f6
Chenzhijun <chenyiyuan00@163.com>
2016-04-29 11:27:17 +0800

    update exception handle

--- src/main/java/com/ccniit/graduation/controller/ErrorController.java
+++ src/main/java/com/ccniit/graduation/controller/ErrorController.java
@@ -1,23 +1,32 @@
 package com.ccniit.graduation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ErrorController {
 
-	protected static final String NOT_AUTH = "/error/403";
 	protected static final String NOT_FOUND = "/error/404";
 
 	@RequestMapping(value = NOT_FOUND, method = RequestMethod.GET)
-	public String notFound(Object model) {
+	public String notFound(ModelMap modelMap, Exception exception) {
 		return NOT_FOUND;
 	}
 
+	protected static final String NOT_AUTH = "/error/403";
+
 	@RequestMapping(value = NOT_AUTH, method = RequestMethod.GET)
-	public String notAuth(Object model) {
+	public String notAuth(ModelMap modelMap, Exception exception) {
 		return NOT_AUTH;
+	}
+
+	protected static final String SERVER_ERROR = "/error/server";
+
+	@RequestMapping(value = SERVER_ERROR, method = RequestMethod.GET)
+	public String serverError(ModelMap modelMap, Exception exception) {
+		return SERVER_ERROR;
 	}
 
 }
--- src/main/java/com/ccniit/graduation/exception/IException.java
+++ src/main/java/com/ccniit/graduation/exception/IException.java
@@ -7,8 +7,14 @@ public class IException extends Exception {
 	 */
 	private static final long serialVersionUID = 5108882443681231991L;
 
+	protected int code;
+
 	public IException(String message) {
 		super(message);
+	}
+
+	public int getCode() {
+		return code;
 	}
 
 }
